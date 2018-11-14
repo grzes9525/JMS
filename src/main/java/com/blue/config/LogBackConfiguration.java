352d4c7b4e465475979f0b5368cbf7a9c8129a00
grzes9525 <jakubiak.grzegorz95@gmail.com>
2018-11-14 14:35:24 +0100

    konfiguracja logów

---
+++ src/main/java/com/blue/config/LogBackConfiguration.java
@@ -0,0 +1,35 @@
+package com.blue.config;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.FileAppender;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import org.slf4j.LoggerFactory;
+
+public class LogBackConfiguration {
+
+    public static Logger createLoggerFor(String string, String file) {
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        PatternLayoutEncoder ple = new PatternLayoutEncoder();
+
+        ple.setPattern("%date %level [%thread] %logger{10} [%file:%line] %msg%n");
+        ple.setContext(lc);
+        ple.start();
+        RollingFileAppender<ILoggingEvent> fileAppender = new RollingFileAppender<>();
+        fileAppender.setFile(file);
+        fileAppender.setEncoder(ple);
+        fileAppender.setContext(lc);
+        //przy 10MB plik się zwinie
+        fileAppender.start();
+
+        Logger logger = (Logger) LoggerFactory.getLogger(string);
+        logger.addAppender(fileAppender);
+        logger.setLevel(Level.INFO);
+        logger.setAdditive(false); /* set to true if root should log too */
+
+        return logger;
+    }
+}
